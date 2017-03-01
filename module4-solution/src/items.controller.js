ec2b9b467fd37d5ae0ad919d9c29767532bad912
Alejandro Gonzalez <aleco@abeeworks.com>
2017-03-01 17:52:10 -0600

    Uploaded solution files

---
+++ module4-solution/src/items.controller.js
@@ -0,0 +1,14 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('ItemsController', ItemsController);
+
+ItemsController.$inject = ['items'];
+function ItemsController(items) {
+  var controller = this;
+  controller.items = items.data.menu_items;
+  controller.category = items.data.category.name;
+}
+
+})();
